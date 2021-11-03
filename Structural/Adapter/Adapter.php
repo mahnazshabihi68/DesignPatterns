6257398a18122534840ad7fca1d390296af93434
m.shabihi <shabihi.mahnaz95@gmail.com>
2021-11-03 11:03:16 -0700

    Change Adapter Code

--- Structural/Adapter/Adapter.php
+++ Structural/Adapter/Adapter.php
@@ -1,13 +1,12 @@
 <?php
-
 /**
- * The Gateway defines the domain-specific used by the client code.
+ * The Target defines the domain-specific interface used by the client code.
  */
-class Gateway
+class Target
 {
-    public function sendPayment($amount)
+    public function request(): string
     {
-        echo "Paying via This Gateway: " . $amount;
+        return "Target: The default target's behavior.";
     }
 }
 
@@ -16,11 +15,11 @@ class Gateway
  * with the existing client code. The Adaptee needs some adaptation before the
  * client code can use it.
  */
-class Mellat
+class Adaptee
 {
-    public function doPayment($amount)
+    public function specificRequest(): string
     {
-        echo "Paying via Mellat Gateway: " . $amount;
+        return ".eetpadA eht fo roivaheb laicepS";
     }
 }
 
@@ -28,40 +27,33 @@ class Mellat
  * The Adapter makes the Adaptee's interface compatible with the Target's
  * interface.
  */
-class PaymentAdapter extends Gateway
+class Adapter extends Target
 {
-    private $mellat;
-    public function __construct(Mellat $mellat)
+    private $adaptee;
+
+    public function __construct(Adaptee $adaptee)
     {
-        $this->mellat = $mellat;
+        $this->adaptee = $adaptee;
     }
 
-    public function sendPayment($amount)
+    public function request(): string
     {
-        $this->mellat->doPayment($amount);
+        return "Adapter: (TRANSLATED) " . strrev($this->adaptee->specificRequest());
     }
 }
-
 
 /**
- * The client code supports all classes that follow the PaymentInterface.
+ * The client code supports all classes that follow the Target interface.
  */
-function clientCode(Gateway $gateway)
+function clientCode(Target $target)
 {
-    echo $gateway->sendPayment(90000);
+    echo $target->request();
 }
-
-
-echo "Client: I can work just fine with the Gateway objects:\n";
-$gateway = new Gateway();
-clientCode($gateway);
+$adaptee = new Adaptee();
+echo "Client: The Adaptee class has a weird interface. See, I don't understand it:\n";
+echo "Adaptee: " . $adaptee->specificRequest();
 echo "\n\n";
 
-echo "Client: I can work just fine with the Mellat objects:\n";
-$mellatGateway = new Mellat();
-$mellatGateway->doPayment(50, 000, 000);
-echo "\n\n";
-
-echo "Client: But I can work with it via the PaymentAdapter:\n";
-$adapter = new PaymentAdapter(new Mellat());
+echo "Client: But I can work with it via the Adapter:\n";
+$adapter = new Adapter($adaptee);
 clientCode($adapter);
